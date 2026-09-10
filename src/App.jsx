c5662d9de94c1dfb3c99768115efc8ae065dc1a7
berefire <berenice.prado.villalobos@gmail.com>
2026-09-09 19:17:24 -0600

    feat: set up routing in App component with Home and CountryDetailPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { Routes, Route } from "react-router-dom";
 import Header from "@/components/Header/Header";
 import Attribution from "@/components/Attribution/Attribution";
+import Home from "@/pages/Home/Home";
+import CountryDetailPage from "@/pages/CountryDetailPage/CountryDetailPage";
 
 function App() {
   return (
     <div className="min-h-dvh flex flex-col bg-white dark:bg-blue-950">
       <Header />
+      <main className="flex-1">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/country/:code" element={<CountryDetailPage />} />
+        </Routes>
+      </main>
       <Attribution />
     </div>
   );
