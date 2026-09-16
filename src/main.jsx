843918d0d9ee05474db9cc53d62d0f6498e227a9
berefire <berenice.prado.villalobos@gmail.com>
2026-09-16 14:46:36 -0600

    fix: update import path for ThemeProvider in main component

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from '@/context/ThemeContext.jsx';
+import { ThemeProvider } from '@/context/ThemeProvider.jsx';
 import App from './App.jsx';
 import "@/styles/index.css";
 
