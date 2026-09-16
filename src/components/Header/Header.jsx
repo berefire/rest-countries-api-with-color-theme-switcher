22d0a3e87fbd7db67c53da27452694e232ef05d1
berefire <berenice.prado.villalobos@gmail.com>
2026-09-16 14:21:47 -0600

    feat: wrap header title in Link component for navigation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import ThemeToggle from "@/components/ThemeToggle/ThemeToggle";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <header className="flex items-center justify-between px-4 md:px-10.25 lg:px-20.25 py-7.5 lg:py-6 bg-white dark:bg-blue-900 shadow-sm dark:shadow-sm font-body">
       <h1 className="font-extrabold leading-[1.2] lg:leading-snug text-sm lg:text-2xl text-gray-900 dark:text-white">
-        Where in the world?
+        <Link to="/" className="no-underline text-inherit focus-ring" >Where in the world?</Link>
       </h1>
       <ThemeToggle />
     </header>
