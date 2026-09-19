4d68c6cdaa4d929b2fe58f229640e450f1b27ba8
berefire <berenice.prado.villalobos@gmail.com>
2026-09-19 01:14:28 -0600

    fix: prevent Back button from exiting the site when there's no in-app history

--- src/pages/CountryDetailPage/CountryDetailPage.jsx
+++ src/pages/CountryDetailPage/CountryDetailPage.jsx
@@ -1,4 +1,4 @@
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams, useNavigate, useLocation } from "react-router-dom";
 import { useCountry } from "@/hooks/useCountry";
 import CountryDetail from "@/components/CountryDetail/CountryDetail";
 import Button from "@/components/Button/Button";
@@ -7,6 +7,7 @@ import { BsArrowLeft } from "react-icons/bs";
 function CountryDetailPage() {
   const { code } = useParams();
   const navigate = useNavigate();
+  const location = useLocation();
   const { country, borderCountries } = useCountry(code);
 
   if (!country) {
@@ -25,7 +26,7 @@ function CountryDetailPage() {
   return (
     <div className="flex justify-center">
       <div className="w-fit flex flex-col items-start gap-16 md:gap-14 lg:gap-20 pbs-10 pbe-13.5 md:pbe-16 xl:px-20.25">
-        <Button onClick={() => navigate(-1)}>
+        <Button onClick={() => navigate(location.key === "default" ? "/" : -1)}>
           <BsArrowLeft /> Back
         </Button>
         <CountryDetail country={country} borderCountries={borderCountries} />
